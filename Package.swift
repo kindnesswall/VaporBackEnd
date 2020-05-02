02ff256828c9a3c9b57bf0bfc073f92cfa2e1263
Amir Hossein Zakizadeh <amirhossein.zakizadeh@gmail.com>
2020-05-02 21:30:21 +0430

    MihaelIsaev/FCM package updated

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-postgresql.git",from:"1.0.0"),
         .package(url: "https://github.com/vapor/auth.git",from:"2.0.0"),
         .package(url: "https://github.com/Jinxiansen/Guardian.git", from: "3.0.0"),
-        .package(url: "https://github.com/MihaelIsaev/FCM.git", from: "0.6.2"),
+        .package(url: "https://github.com/MihaelIsaev/FCM.git", from: "1.1.0"),
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor","FluentPostgreSQL","Authentication","Guardian","FCM"]),
