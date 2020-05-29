b8b370b00e46daf6dca2ade527fa2517710b5055
Amir Hossein Zakizadeh <amirhossein.zakizadeh@gmail.com>
2020-05-29 17:45:06 +0430

    Application version: improvements 2.1

--- Sources/App/ModelSeeds/ApplicationVersionSeed.swift
+++ Sources/App/ModelSeeds/ApplicationVersionSeed.swift
@@ -12,8 +12,8 @@ final class ApplicationVersionSeed: Migration {
     typealias Database = PostgreSQLDatabase
     
     static let seeds = [
-        ApplicationVersion(platform: .iOS, input: Inputs.ApplicationVersion(availableVersionName: "1.0.0", availableVersionCode: 1, requiredVersionName: "1.0.0", requiredVersionCode: 1)),
-        ApplicationVersion(platform: .android, input: Inputs.ApplicationVersion(availableVersionName: "1.0.0", availableVersionCode: 1, requiredVersionName: "1.0.0", requiredVersionCode: 1))
+        ApplicationVersion(platform: .iOS, input: Inputs.ApplicationVersion(availableVersionName: "1.0.0", availableVersionCode: 1, requiredVersionName: "1.0.0", requiredVersionCode: 1, downloadLink: nil)),
+        ApplicationVersion(platform: .android, input: Inputs.ApplicationVersion(availableVersionName: "1.0.0", availableVersionCode: 1, requiredVersionName: "1.0.0", requiredVersionCode: 1, downloadLink: nil))
     ]
     
     static func prepare(on conn: PostgreSQLConnection) -> EventLoopFuture<Void> {
