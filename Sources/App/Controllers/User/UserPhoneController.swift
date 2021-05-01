be429028a37337babcdd08b12a13052b42643855
Amir Hossein Zakizadeh <amirhossein.zakizadeh@gmail.com>
2021-05-01 23:01:38 +0430

    Add UserPhoneController: Refactor

--- Sources/App/Controllers/User/UserPhoneController.swift
+++ Sources/App/Controllers/User/UserPhoneController.swift
@@ -10,24 +10,21 @@ import Vapor
 final class UserPhoneController {
     
     func checkPhoneNumberAccessibility(_ req: Request) throws -> Future<Bool> {
-        let auth = try req.requireAuthenticated(User.self)
-        let isAdmin = auth.isAdmin
-        let isCharity = auth.isCharity
-        let userId = try req.parameters.next(Int.self)
-        
-        return User.get(userId, on: req).map { user in
-            
-            let isPhoneVisibleForAll = user.isPhoneVisibleForAll ?? false
-            let isPhoneVisibleForCharities = user.isPhoneVisibleForCharities ?? false
-            
-            guard isAdmin || isPhoneVisibleForAll || (isCharity && isPhoneVisibleForCharities) else {
-                return false
-            }
-            return true
+        try getUserIfPhoneNumberIsAccessible(req).map { user in
+            return user != nil
         }
     }
     
     func getPhoneNumber(_ req: Request) throws -> Future<Outputs.UserPhoneNumber> {
+        try getUserIfPhoneNumberIsAccessible(req).map { user in
+            guard let user = user else {
+                throw Abort(.phoneNumberIsNotAccessible)
+            }
+            return Outputs.UserPhoneNumber(phoneNumber: user.phoneNumber)
+        }
+    }
+    
+    private func getUserIfPhoneNumberIsAccessible(_ req: Request) throws -> Future<User?> {
         let auth = try req.requireAuthenticated(User.self)
         let isAdmin = auth.isAdmin
         let isCharity = auth.isCharity
@@ -39,10 +36,9 @@ final class UserPhoneController {
             let isPhoneVisibleForCharities = user.isPhoneVisibleForCharities ?? false
             
             guard isAdmin || isPhoneVisibleForAll || (isCharity && isPhoneVisibleForCharities) else {
-                throw Abort(.phoneNumberIsNotAccessible)
+                return nil
             }
-            
-            return Outputs.UserPhoneNumber(phoneNumber: user.phoneNumber)
+            return user
         }
     }
 }
