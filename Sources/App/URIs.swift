f01a5525778812bf900567291f4745ca14b8db9a
Amir Hossein Zakizadeh <amirhossein.zakizadeh@gmail.com>
2021-06-11 17:06:05 +0430

    Upgrading to v4: Routes (progress 3)

--- Sources/App/URIs.swift
+++ Sources/App/URIs.swift
@@ -34,6 +34,10 @@ class URIs {
         return absolutePath
     }
     
+    func appendID(to basePath: [PathComponent]) -> [PathComponent] {
+        append(to: basePath, path: [id])
+    }
+    
     private let id = ":id"
     
     var root: [PathComponent] {
@@ -45,6 +49,9 @@ class URIs {
     var gifts : [PathComponent] {
         makeAPI(path: ["gifts"])
     }
+    var gifts_id : [PathComponent] {
+        appendID(to: gifts)
+    }
     var categories : [PathComponent] {
         makeAPI(path: ["categories"])
     }
@@ -54,11 +61,20 @@ class URIs {
     var province : [PathComponent] {
         makeAPI(path: ["provinces"])
     }
+    var province_id : [PathComponent] {
+        appendID(to: province)
+    }
     var city : [PathComponent] {
         makeAPI(path: ["cities"])
     }
+    var city_id : [PathComponent] {
+        appendID(to: city)
+    }
     var region: [PathComponent] {
         makeAPI(path: ["regions"])
+    }
+    var region_id: [PathComponent] {
+        appendID(to: region)
     }
     var register : [PathComponent] {
         makeAPI(path: ["register"])
@@ -90,6 +106,9 @@ class URIs {
     var chat_start : [PathComponent] {
         append(to: chat, path: ["start"])
     }
+    var chat_start_id : [PathComponent] {
+        appendID(to: chat_start)
+    }
     var chat_contacts : [PathComponent] {
         append(to: chat, path: ["contacts"])
     }
@@ -108,8 +127,14 @@ class URIs {
     var chat_block : [PathComponent] {
         append(to: chat, path: ["block"])
     }
+    var chat_block_id : [PathComponent] {
+        appendID(to: chat_block)
+    }
     var chat_unblock : [PathComponent] {
         append(to: chat, path: ["unblock"])
+    }
+    var chat_unblock_id : [PathComponent] {
+        appendID(to: chat_unblock)
     }
     var gifts_register : [PathComponent] {
         append(to: gifts, path: ["register"])
@@ -117,14 +142,26 @@ class URIs {
     var gifts_userRegistered : [PathComponent] {
         append(to: gifts, path: ["userRegistered"])
     }
+    var gifts_userRegistered_id : [PathComponent] {
+        appendID(to: gifts_userRegistered)
+    }
     var gifts_userDonated: [PathComponent] {
         append(to: gifts, path: ["userDonated"])
+    }
+    var gifts_userDonated_id: [PathComponent] {
+        appendID(to: gifts_userDonated)
     }
     var gifts_userReceived : [PathComponent] {
         append(to: gifts, path: ["userReceived"])
     }
+    var gifts_userReceived_id : [PathComponent] {
+        appendID(to: gifts_userReceived)
+    }
     var gifts_todonate : [PathComponent] {
         append(to: gifts, path: ["todonate"])
+    }
+    var gifts_todonate_id : [PathComponent] {
+        appendID(to: gifts_todonate)
     }
     var image_upload : [PathComponent] {
         makeAPI(path: ["image", "upload"])
@@ -132,8 +169,14 @@ class URIs {
     var gifts_accept : [PathComponent] {
         append(to: gifts, path: ["accept"])
     }
+    var gifts_accept_id : [PathComponent] {
+        appendID(to: gifts_accept)
+    }
     var gifts_reject : [PathComponent] {
         append(to: gifts, path: ["reject"])
+    }
+    var gifts_reject_id : [PathComponent] {
+        appendID(to: gifts_reject)
     }
     var gifts_review : [PathComponent] {
         append(to: gifts, path: ["review"])
@@ -146,6 +189,9 @@ class URIs {
     }
     var users_denyAccess : [PathComponent] {
         append(to: users, path: ["denyAccess"])
+    }
+    var users_denyAccess_id : [PathComponent] {
+        appendID(to: users_denyAccess)
     }
     var users_list : [PathComponent] {
         append(to: users, path: ["list"])
@@ -161,6 +207,9 @@ class URIs {
     }
     var users_statistics : [PathComponent] {
         append(to: users, path: ["statistics"])
+    }
+    var users_statistics_id : [PathComponent] {
+        appendID(to: users_statistics)
     }
     var users_statistics_list : [PathComponent] {
         append(to: users_statistics, path: ["list"])
@@ -183,14 +232,26 @@ class URIs {
     var charity_info_user : [PathComponent] {
         append(to: charity, path: ["info", "user"])
     }
+    var charity_info_user_id : [PathComponent] {
+        appendID(to: charity_info_user)
+    }
     var charity_user : [PathComponent] {
         append(to: charity, path: ["user"])
+    }
+    var charity_user_id : [PathComponent] {
+        appendID(to: charity_user)
     }
     var charity_accept_user : [PathComponent] {
         append(to: charity, path: ["accept", "user"])
     }
+    var charity_accept_user_id : [PathComponent] {
+        appendID(to: charity_accept_user)
+    }
     var charity_reject_user : [PathComponent] {
         append(to: charity, path: ["reject", "user"])
+    }
+    var charity_reject_user_id : [PathComponent] {
+        appendID(to: charity_reject_user)
     }
     var charity_review : [PathComponent] {
         append(to: charity, path: ["review"])
@@ -201,14 +262,23 @@ class URIs {
     var gifts_request : [PathComponent] {
         append(to: gifts, path: ["request"])
     }
+    var gifts_request_id : [PathComponent] {
+        appendID(to: gifts_request)
+    }
     var gifts_request_status : [PathComponent] {
         append(to: gifts_request, path: ["status"])
+    }
+    var gifts_request_status_id : [PathComponent] {
+        appendID(to: gifts_request_status)
     }
     var donate : [PathComponent] {
         makeAPI(path: ["donate"])
     }
     var profile : [PathComponent] {
         makeAPI(path: ["profile"])
+    }
+    var profile_id : [PathComponent] {
+        appendID(to: profile)
     }
     var sendPush : [PathComponent] {
         makeAPI(path: ["sendPush"])
@@ -231,11 +301,17 @@ class URIs {
     var sponsors : [PathComponent] {
         makeAPI(path: ["sponsors"])
     }
+    var sponsors_id : [PathComponent] {
+        appendID(to: sponsors)
+    }
     var sponsors_list : [PathComponent] {
         append(to: sponsors, path: ["list"])
     }
     var rating : [PathComponent] {
         makeAPI(path: ["rating"])
+    }
+    var rating_id : [PathComponent] {
+        appendID(to: rating)
     }
     var phone_visibility: [PathComponent] {
         makeAPI(path: ["phone", "visibility"])
@@ -243,8 +319,14 @@ class URIs {
     var phone_visibility_check: [PathComponent] {
         append(to: phone_visibility, path: ["check"])
     }
+    var phone_visibility_check_id: [PathComponent] {
+        appendID(to: phone_visibility_check)
+    }
     var phone_visibility_access: [PathComponent] {
         append(to: phone_visibility, path: ["access"])
+    }
+    var phone_visibility_access_id: [PathComponent] {
+        appendID(to: phone_visibility_access)
     }
     var phone_visibility_setting: [PathComponent] {
         append(to: phone_visibility, path: ["setting"])
