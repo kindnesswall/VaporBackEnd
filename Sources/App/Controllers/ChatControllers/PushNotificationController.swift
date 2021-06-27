e8e58960ca102a468c098ab643b4b9ebf5667f27
Amir Hossein Zakizadeh <amirhossein.zakizadeh@gmail.com>
2021-06-27 18:08:39 +0430

    Upgrading to v4: Controllers - PushNotificationController

--- Sources/App/Controllers/ChatControllers/PushNotificationController.swift
+++ Sources/App/Controllers/ChatControllers/PushNotificationController.swift
@@ -16,49 +16,56 @@ class PushNotificationController {
         let userToken = try req.auth.require(Token.self)
         let userTokenId = try userToken.getId()
         
-        return try req.content.decode(Inputs.UserPushNotification.self).flatMap { input in
+        let input = try req.content.decode(Inputs.UserPushNotification.self)
+        
+        guard let _ = PushNotificationType(rawValue: input.type) else {
+            throw Abort(.wrongPushNotificationType)
+        }
+        
+        return UserPushNotification.hasFound(
+            input: input,
+            conn: req.db).flatMap { found in
             
-            guard let _ = PushNotificationType(rawValue: input.type) else {
-                throw Abort(.wrongPushNotificationType)
+            if let found = found {
+                found.userId = userId
+                found.userTokenId = userTokenId
+                return found.save(on: req.db)
+                    .transform(to: .ok)
+            } else {
+                let item = UserPushNotification(
+                    userId: userId,
+                    userTokenId: userTokenId,
+                    input: input)
+                return item.save(on: req.db)
+                    .transform(to: .ok)
             }
-            
-            return UserPushNotification.hasFound(input: input, conn: req).flatMap({ found -> EventLoopFuture<UserPushNotification> in
-                
-                if let found = found {
-                    found.userId = userId
-                    found.userTokenId = userTokenId
-                    return found.save(on: req)
-                } else {
-                    let item = UserPushNotification(userId: userId, userTokenId: userTokenId, input: input)
-                    return item.save(on: req)
-                }
-            }).transform(to: .ok)
-            
         }
     }
     
-    
     func sendPush(_ req: Request) throws -> EventLoopFuture<HTTPStatus> {
-        return try req.content.decode(SendPushInput.self).map { input in
-            let payload = SamplePushPayload()
-            try PushNotificationController.sendPush(req, userId: input.userId, title: input.title, body: input.body, payload: payload)
-            return .ok
-        }
+        let input = try req.content.decode(SendPushInput.self)
+        let payload = SamplePushPayload()
+        try PushNotificationController.sendPush(req, userId: input.userId, title: input.title, body: input.body, payload: payload)
+        return req.db.makeSucceededFuture(.ok)
     }
     
     
     static func sendPush<T: PushPayloadable>(_ req: Request, userId:Int, title:String?, body:String, payload: T) throws {
         
-        UserPushNotification.findAllTokens(userId: userId, conn: req).map { allTokens in
-            
+        UserPushNotification.findAllTokens(
+            userId: userId,
+            conn: req.db).flatMapThrowing { allTokens in
             for token in allTokens {
-                try sendPush(req, token: token, title: title, body: body, payload: payload)?.catch({ error in
-                    print(error)
-                })
+                try sendPush(
+                    req,
+                    token: token,
+                    title: title,
+                    body: body,
+                    payload: payload)?
+                    .whenFailure { print($0) }
             }
-        }.catch { error in
-            print(error)
         }
+        .whenFailure { print($0) }
     }
     
     static func sendPush<T: PushPayloadable>(_ req: Request, token: UserPushNotification, title:String?, body:String, payload: T) throws -> EventLoopFuture<HTTPStatus>? {
@@ -103,8 +110,7 @@ class PushNotificationController {
         }).transform(to: .ok)
     }
     
-    private static func sendFirebasePush(_ req: Request, token: String, title: String?, body: String, content: PushPayloadContent?, click_action: String?) throws -> EventLoopFuture<HTTPStatus> {
-        let fcm = try req.make(FCM.self)
+    private static func sendFirebasePush(_ req: Request, token: String, title: String?, body: String, content: PushPayloadContent?, click_action: String?) -> EventLoopFuture<HTTPStatus> {
         
         //TODO: More study about restricted_package_name field in FCMMessage (Android only)
         
@@ -120,9 +126,9 @@ class PushNotificationController {
         if let click_action = click_action {
             message.data["click_action"] = click_action
         }
-
-        return try fcm.sendMessage(req.client(), message: message).map({ output in
+        
+        return req.fcm.send(message, on: req.eventLoop).map { output in
             print(output)
-        }).transform(to: .ok)
+        }.transform(to: .ok)
     }
 }
